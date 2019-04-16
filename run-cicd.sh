363c695b8740e4908680eebfc3c39769ec20200c
KipCrossing <kip.crossing@gmail.com>
2019-04-17 09:52:52 +1000

    Get cicd rough working

--- run-cicd.sh
+++ run-cicd.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-echo "git pull"
-git pull
-echo "Run main.py once "
-python3 main.py
+
+while true; do
+  echo "git pull"
+  git pull
+  echo "Run main.py"
+  python3 projectbot.py
+  sleep 600
+  pkill -9 -f projectbot.py
+  echo "Killed and loop"
+done
