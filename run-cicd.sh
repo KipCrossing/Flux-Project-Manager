0416176186e1c12f06f4ac7fcc996ecaed6cc731
KipCrossing <kip.crossing@gmail.com>
2019-04-17 10:16:12 +1000

    Final touches on cicd

--- run-cicd.sh
+++ run-cicd.sh
@@ -4,9 +4,10 @@
 while true; do
   echo "git pull"
   git pull
-  echo "Run main.py"
-  python3 projectbot.py
-  sleep 600
+  echo "Run projectbot.py"
+  python3 projectbot.py &
+  sleep 295
   pkill -9 -f projectbot.py
   echo "Killed and loop"
+  sleep 5
 done
